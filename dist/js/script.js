853010ee56f4420087572a99b1a94a6b0651ae8a
Blaxk <deadman962011@gmail.com>
2021-06-22 16:14:34 +0300

    BlaxkWork2

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+$(document).ready(function(){
+
+    setTimeout(function(){
+        //Categories Silder
+        // eslint-disable-next-line no-undef
+        $('.category-carousel').flickity({
+            // options
+            "groupCells": 4  
+          });
+    },20000)
+
+})
+
