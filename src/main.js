73970c8c2d99d4705d35774d7781946b723a1e2b
Blaxk <Blaxk@Blaxk.cc>
2021-07-04 13:21:17 -0700

    7/4-build4

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import https from 'https'
 
 import axios from 'axios'
 let username = "ck_6a1427f5d435ec0a42b8b14c946f24faaf970c18";
 let password = "cs_ec4bd419caf66135ecacc5df3410a7ec03a5fcfb";
 let authHeader = `Basic ${window.btoa(`${username}:${password}`)}`;
 axios.defaults.headers.common["Authorization"] = authHeader;
+axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false })
 Vue.config.productionTip = false
 
 new Vue({
