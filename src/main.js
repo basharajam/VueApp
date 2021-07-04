3bd499b8bc50f52aa01188ed9993b13c4530f6c6
Blaxk <Blaxk@Blaxk.cc>
2021-07-04 04:34:08 -0700

    7/4-build5

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import store from './store'
 import https from 'https'
 
 import axios from 'axios'
-let username = "ck_6a1427f5d435ec0a42b8b14c946f24faaf970c18";
-let password = "cs_ec4bd419caf66135ecacc5df3410a7ec03a5fcfb";
+let username = "ck_dd5dacfe7d6b41892c0cbe192f8a553b264afdd6";
+let password = "cs_a495e0fca65ed92130dddb0633370dd826658a4c";
 let authHeader = `Basic ${window.btoa(`${username}:${password}`)}`;
 axios.defaults.headers.common["Authorization"] = authHeader;
 axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false })
