99b90a545a860418e6494c414257177573f04d3e
Blaxk <Blaxk@Blaxk.cc>
2021-07-11 10:20:27 +0300

    7/11-build

--- src/store/modules/Products.js
+++ src/store/modules/Products.js
@@ -53,7 +53,7 @@ const actions = {
     getRecentProd({commit}){
         
         
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?featured = true&per_page=8';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?featured = true& per_page=8';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -72,7 +72,7 @@ const actions = {
     getProdByTax({commit}){
         
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=699&per_page=12';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=699&per_page=12&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -89,7 +89,7 @@ const actions = {
     },
     getProdByTax0({commit}){
 
-        var SetUrl = process.env.VUE_APP_BASEURL+'products?tag=718&per_page=12';
+        var SetUrl = process.env.VUE_APP_BASEURL+'products?tag=718&per_page=12&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -111,7 +111,7 @@ const actions = {
 
     getProdOffers({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=679&per_page=12';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=679&per_page=12&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -129,7 +129,7 @@ const actions = {
 
     getProdBestSell({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=672&per_page=8';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=672&per_page=8&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -148,7 +148,7 @@ const actions = {
 
     getProdMostPopular({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=719&per_page=8';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=719&per_page=8&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -167,7 +167,7 @@ const actions = {
 
     getProdByTax1({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=720&per_page=6';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=720&per_page=6&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -186,7 +186,7 @@ const actions = {
 
     getProdByTax2({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=695&per_page=12';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=695&per_page=12&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -205,7 +205,7 @@ const actions = {
 
     getProdByTax3({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=731&per_page=12';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=731&per_page=12&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -224,7 +224,7 @@ const actions = {
     },
     getProdByTax4({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=705&per_page=12';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=705&per_page=12&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -242,7 +242,7 @@ const actions = {
     },
     getProdByTax5({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=723&per_page=10';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=723&per_page=10&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -261,7 +261,7 @@ const actions = {
 
     getProdByTax6({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=717&per_page=12';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=717&per_page=12&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -279,7 +279,7 @@ const actions = {
     },
     getProdByTax7({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=716&per_page=12';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=716&per_page=12&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -297,7 +297,7 @@ const actions = {
     },
     getProdByTax8({commit}){
 
-        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=703&per_page=12';
+        var SetUrl=process.env.VUE_APP_BASEURL+'products?tag=703&per_page=12&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -335,7 +335,7 @@ const actions = {
 
 
         //Do Requset 
-        var SetUrl = process.env.VUE_APP_BASEURL+'products?tag=696&per_page=8';
+        var SetUrl = process.env.VUE_APP_BASEURL+'products?tag=696&per_page=8&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -353,7 +353,7 @@ const actions = {
 
     getProdDecRope({commit}){
 
-        var Seturl = process.env.VUE_APP_BASEURL+'products?tag=703&per_page=4';
+        var Seturl = process.env.VUE_APP_BASEURL+'products?tag=703&per_page=4&status=publish';
 
         axios.get(Seturl).then(function(response){
 
@@ -374,7 +374,7 @@ const actions = {
 
     getProdDecSticker({commit}){
 
-        var SetUrl = process.env.VUE_APP_BASEURL+'products?tag=704&per_page=4';
+        var SetUrl = process.env.VUE_APP_BASEURL+'products?tag=704&per_page=4&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -394,7 +394,7 @@ const actions = {
 
     getProdDecIns({commit}){
 
-        var SetUrl = 'https://alyaman.com/wp-json/wc/v3/products?tag=705&per_page=4';
+        var SetUrl = 'https://alyaman.com/wp-json/wc/v3/products?tag=705&per_page=4&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
@@ -414,7 +414,7 @@ const actions = {
 
     getProdDecTag({commit}){
 
-        var SetUrl = 'https://alyaman.com/wp-json/wc/v3/products?tag=707&per_page=4';
+        var SetUrl = 'https://alyaman.com/wp-json/wc/v3/products?tag=707&per_page=4&status=publish';
         axios.get(SetUrl).then(function(response){
 
             if(response.status != 200){
