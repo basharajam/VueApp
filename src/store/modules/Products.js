dfde8872ace73fd02ce89f488b95614f05e61d9c
Blaxk <Blaxk@Blaxk.cc>
2021-08-20 14:05:55 +0300

    8/20-4

--- src/store/modules/Products.js
+++ src/store/modules/Products.js
@@ -84,8 +84,12 @@ const actions = {
         }
         //http://127.0.0.1:8000/api/Products/USD/UAE
         //https://wordpress-608610-2061089.cloudwaysapps.com/
+        
         //var setUrl ='http://127.0.0.1:8000/api/Products/'+cur+'/'+country;
-        var setUrl ='https://phplaravel-608610-2049275.cloudwaysapps.com/api/Products/'+cur+'/'+country;
+        //X
+        //var setUrl ='https://phplaravel-608610-2049275.cloudwaysapps.com/api/Products/'+cur+'/'+country;
+        //Or
+        var setUrl= 'https://phplaravel-608610-2037155.cloudwaysapps.com/api/Products/'+cur+'/'+country;
           axios.get(setUrl).then(function(response){
     
             if(response.status != 200){
